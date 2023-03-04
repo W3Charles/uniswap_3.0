650de48590bf83ee09c4aee9c5160b43ce82abb9
W3Charles <www3charles@gmail.com>
2023-03-04 05:47:25 -0500

    feat: add index.js file to import  components
    - Import all components created so far
    
    - Export components at the bottom

---
+++ Components/index.js
@@ -0,0 +1,15 @@
+import HeroSection from "./HeroSection/HeroSection";
+import Modal from "./Modal/Modal";
+import NavBar from "./NavBar/NavBar";
+import SearchToken from "./SearchToken/SearchToken";
+import Toggle from "./Toggle/Toggle";
+import TokenList from "./TokenList/TokenList";
+
+export {
+    HeroSection, 
+    Modal, 
+    NavBar, 
+    SearchToken, 
+    Toggle, 
+    TokenList
+}
