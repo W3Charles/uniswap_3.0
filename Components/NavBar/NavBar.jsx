3e4038459fdafdc7f2ec65a379f5ad8df3ed2fe3
W3Charles <www3charles@gmail.com>
2023-03-11 01:39:07 -0500

    build: import internal dependencies

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect, useContext } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+//INTERNAL IMPORTS
+import Style from './NavBar.module.css';
+import images from '../../assets';
+import { Modal, TOkenList } from '../index';
+
 const NavBar = () => {
   return (
     <div>NavBar</div>
