64de4840cbbcddac94f502c8cdafe9f629bf77cf
W3Charles <www3charles@gmail.com>
2023-03-12 01:43:10 -0500

    build: import Modal css Style & images

--- Components/Modal/Modal.jsx
+++ Components/Modal/Modal.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
+//INTERNAL IMPORTS
+import Style from './Modal.module.css';
+import images from './,,/../assets';
+
 const Modal = () => {
   return (
     <div>Modal</div>
