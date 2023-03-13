183a350f1acf63e2c098702b0676a1c7e65bdc43
W3Charles <www3charles@gmail.com>
2023-03-13 04:16:54 -0400

    style: remove exclamation
    - Testing is complete for now

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -74,7 +74,7 @@ const NavBar = () => {
       </div>
 
       {/* TOKENLIST COMPONENT */}
-      {!openTokenBox && (
+      {openTokenBox && (
         <TokenList tokenDate="hey" setOpenTokenBox={setOpenTokenBox} />
       )}
     </div>
