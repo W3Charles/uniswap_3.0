ea9410293ede567da13370507178ec0aa8bf0be6
W3Charles <www3charles@gmail.com>
2023-03-13 01:59:23 -0400

    feat: create demo array variable for token list

--- Components/TokenList/TokenList.jsx
+++ Components/TokenList/TokenList.jsx
@@ -6,6 +6,7 @@ import Style from './TokenList.module.css';
 import images from '../../assets';
 
 const TokenList = ({ tokenDate, setOpenTokenBox }) => {
+  const data = [1, 2, 3, 4, 5, 6, 7];
   return (
     <div>TokenList</div>
   )
