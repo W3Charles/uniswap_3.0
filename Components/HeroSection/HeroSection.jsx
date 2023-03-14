115b3c843086fa915a6cf6124bc6a6cff6af7e3e
W3Charles <www3charles@gmail.com>
2023-03-14 04:19:47 -0400

    build: import internal & external dependencies

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+//INTERNAL IMPORTS
+import Style from './HeroSection.module.css';
+import images from '../../assets';
+import { Token, SearchToken } from '../index';
 
 const HeroSection = ({ accounts, tokenData }) => {
   return (
