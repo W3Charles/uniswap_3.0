1c4461b793f7cb3f81b0dfd0408ec89d22932433
W3Charles <www3charles@gmail.com>
2023-03-14 04:49:06 -0400

    feat: add & declare useState variables

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -7,6 +7,24 @@ import images from '../../assets';
 import { Token, SearchToken } from '../index';
 
 const HeroSection = ({ accounts, tokenData }) => {
+  //USESTATE
+  const [openSetting, setOpenSetting] = useState(false);
+  const [openToken, setOpenToken] = useState(false);
+  const [openTokensTwo, setOpenTokensTwo] = useState(false);
+
+  //TOKEN 1
+  const [tokenOne, setTokeOne] = useState({
+    name: "",
+    image: "",
+  });
+
+  //TOKEN 2
+  const [tokenTwo, setTokeTwo] = useState({
+    name: "",
+    image: "",
+  });
+
+  
   return (
     <div>HeroSection</div>
   )
