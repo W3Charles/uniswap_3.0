6538adce548b29f1e7a215eb2c1a3a303f8b3f46
W3Charles <www3charles@gmail.com>
2023-03-16 05:29:18 -0400

    feat: add Swap section
    - Create two dynamic blocks to store data of 2 tokens user wants to swap

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -24,9 +24,80 @@ const HeroSection = ({ accounts, tokenData }) => {
     image: "",
   });
 
-  
+  //JSX
   return (
-    <div>HeroSection</div>
+    <div className={Style.HeroSection}>
+      <div className={Style.HeroSection_box}>
+        <div className={Style.HeroSection_box_heading}>
+          <p>Swap</p>
+          <div className={Style.HeroSection_box_heading_img}>
+            <Image 
+              src={images.close}
+              alt="image"
+              width={50}
+              height={50}
+              onClick={() => setOpenSetting(true)}
+            />
+          </div>
+        </div>
+
+        <div className={Style.HeroSection_box_input}>
+          <input type="number" placeholder="0" />
+          <button onClick={() => openToken(true)}>
+            <Image 
+              src={images.image || images.etherlogo} 
+              width={20}
+              height={20}
+              alt="ether"
+            />
+            {tokenOne.name || "ETH"}
+            <small>9474</small>
+          </button>
+        </div>
+
+        <div className={Style.HeroSection_box_input}>
+          <input type="number" placeholder="0" />
+          <button onClick={() => openToken(true)}>
+            <Image 
+              src={tokenTwo.image || images.etherlogo} 
+              width={20}
+              height={20}
+              alt="ether"
+            />
+            {tokenTwo.name || "ETH"}
+            <small>9474</small>
+          </button>
+        </div>
+
+        {accounts ? (
+          <button className={Style.HeroSection_box_btn}>Connect Wallet</button>
+        ): (
+          <button 
+            className={Style.HeroSection_box_btn}
+            onClick={() => {}}
+          >
+            Swap
+          </button>
+        )}
+      </div>
+
+      {openSetting && <Token openSetting={openSetting} />}
+
+      {openToken && (
+        <SearchToken 
+          openToken={setOpenToken}
+          tokens={setTokenOne}
+          tokenData={tokenData}
+        />
+      )}
+      {openToken && (
+        <SearchToken 
+          openToken={setOpenTokensTwo}
+          tokens={setTokenTwo}
+          tokenData={tokenData}
+        />
+      )}
+    </div>
   )
 }
 
