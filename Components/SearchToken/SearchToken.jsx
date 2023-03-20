f48c0366b11c98671b2082c09bef30ecfe136898
W3Charles <www3charles@gmail.com>
2023-03-20 01:41:58 -0400

    build: import SearchToken dependencies

--- Components/SearchToken/SearchToken.jsx
+++ Components/SearchToken/SearchToken.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import image from 'next/image';
+
+//INTERNAL IMPORTS
+import Style from './SearchToken.module.css';
+import images from '../../assets';
 
 const SearchToken = () => {
   return (
