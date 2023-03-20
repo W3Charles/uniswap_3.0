815640431e0ec3639840e941445aa242373571ed
W3Charles <www3charles@gmail.com>
2023-03-20 01:59:58 -0400

    feat: add active useState and coin demo data

--- Components/SearchToken/SearchToken.jsx
+++ Components/SearchToken/SearchToken.jsx
@@ -6,6 +6,49 @@ import Style from './SearchToken.module.css';
 import images from '../../assets';
 
 const SearchToken = ({ openToken, tokens, tokenData }) => {
+  //USESTATE
+  const [active, setActive] = useState(1);
+
+  //DEMO DATA
+  const coin = [
+    {
+      img: images.ether,
+      name: "ETH"
+    },
+    {
+      img: images.ether,
+      name: "DAI"
+    },
+    {
+      img: images.ether,
+      name: "DOG"
+    },
+    {
+      img: images.ether,
+      name: "FUN"
+    },
+    {
+      img: images.ether,
+      name: "WETH9"
+    },
+    {
+      img: images.ether,
+      name: "UNI"
+    },
+    {
+      img: images.ether,
+      name: "TIME"
+    },
+    {
+      img: images.ether,
+      name: "LOO"
+    },
+    {
+      img: images.ether,
+      name: "POO"
+    },
+  ]
+
   return (
     <div>SearchToken</div>
   )
