f314554d419cf054d4972ee4d7b060c9134f25bf
W3Charles <www3charles@gmail.com>
2023-03-27 02:10:26 -0400

    build: import DAI, WETH9 & USDC tokens

--- test/SingleSwap.js
+++ test/SingleSwap.js
@@ -1,2 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+
+const DAI = "0x2bcAE8205a77dabB2479CF2c85ded7d963101B86";
+const WETH9 = "0xEF1DACBce5194C668BEe55f2ca599F366709db0C";
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
