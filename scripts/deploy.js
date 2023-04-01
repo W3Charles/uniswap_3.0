700a59e25ab573b4b49026b72dc7f00eca2e9a2b
W3Charles <www3charles@gmail.com>
2023-04-01 04:14:02 -0400

    ci: edit deploy.js & add Token deploy scripts
    - Add deploy scripts for BooToken & LifeToken

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,23 @@
 const hre = require("hardhat");
 
 async function main() {
+  //ERC20 BOO TOKEN
+  const BooToken = await hre.ethers.getContractFactory("Lock");
+  const booToken = await BooToken.deploy();
+  await booToken.deployed();
+  console.log(`BOO deployed to${booToken.address}`);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy();
+  // console.log(
+  //   `Lock with ${ethers.utils.formatEther(
+  //     lockedAmount
+  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${booToken.address}`
+  // );
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  //ERC20 LIFE TOKEN
+  const LifeToken = await hre.ethers.getContractFactory("Lock");
+  const lifeToken = await LifeToken.deploy();
+  await lifeToken.deployed();
+  console.log(`Life deployed to${lifeToken.address}`);
 }
 
 main().catch((error) => {
