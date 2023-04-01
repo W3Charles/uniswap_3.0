c3c74e7e8ab340bd1d7bf04ad82efe4811d0c9c0
W3Charles <www3charles@gmail.com>
2023-04-01 02:21:07 -0400

    feat: add BooToken ERC20 Token Contract

--- contracts/ERC20Boo.sol
+++ contracts/ERC20Boo.sol
@@ -2,3 +2,10 @@
 pragma solidity >=0.7.0 < 0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BooToken is ERC20 {
+
+    constructor() ERC20("LF", "Boo"){
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+}
