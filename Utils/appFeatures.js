6b5e33380aa9834d8c4ce0c7797cee7a163585d1
W3Charles <www3charles@gmail.com>
2023-04-03 05:31:33 -0400

    build: import tokens & ABIs from constants folder

--- Utils/appFeatures.js
+++ Utils/appFeatures.js
@@ -1,2 +1,15 @@
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
+
+import {
+    BooTokenAddress,
+    BooTokenABI,
+    LifeTokenAddress,
+    LifeTokenABI,
+    SingleSwapTokenAddress,
+    SingleSwapTokenABI,
+    SwapMultiHopAddress,
+    SwapMultiHopABI,
+    IWETHAddress,
+    IWETHABI,
+  } from '../Context/constants';
