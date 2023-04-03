aea81b4c5beaac34df938132dfdcd51d2b420689
W3Charles <www3charles@gmail.com>
2023-04-03 04:19:08 -0400

    feat: add token export variables & ABIs

--- Context/constants.js
+++ Context/constants.js
@@ -3,7 +3,24 @@ import lifeToken from "./LifeToken.json";
 import singleSWapToken from "./SingleSwapToken.json";
 import swapMultiHop from "./SwapMultiHop.json";
 
-// BOO deployed to0x9e7F7d0E8b8F38e3CF2b3F7dd362ba2e9E82baa4
-// Life deployed to0x0724F18B2aA7D6413D3fDcF6c0c27458a8170Dd9
-// SingleSwapToken deployed to0xE7FF84Df24A9a252B6E8A5BB093aC52B1d8bEEdf
-// SwapMultiHop deployed to0x6D712CB50297b97b79dE784d10F487C00d7f8c2C
+// BOO TOKEN
+export const BooTokenAddress = "0x9e7F7d0E8b8F38e3CF2b3F7dd362ba2e9E82baa4";
+export const BooTokenABI = booToken.abi;
+
+// LIFE TOKEN
+export const LifeTokenAddress = "0x0724F18B2aA7D6413D3fDcF6c0c27458a8170Dd9";
+export const LifeTokenABI = lifeToken.abi;
+
+// SINGLE SWAP TOKEN
+export const SingleSwapTokenAddress =
+  "0xE7FF84Df24A9a252B6E8A5BB093aC52B1d8bEEdf";
+export const SingleSwapTokenABI = singleSwapToken.abi;
+
+// SWAP MULTIHOP
+export const SwapMultiHopAddress = "0x6D712CB50297b97b79dE784d10F487C00d7f8c2C";
+export const SwapMultiHopABI = swapMultiHop.abi;
+
+// BOO deployed to 0x9e7F7d0E8b8F38e3CF2b3F7dd362ba2e9E82baa4
+// Life deployed to 0x0724F18B2aA7D6413D3fDcF6c0c27458a8170Dd9
+// SingleSwapToken deployed to 0xE7FF84Df24A9a252B6E8A5BB093aC52B1d8bEEdf
+// SwapMultiHop deployed to 0x6D712CB50297b97b79dE784d10F487C00d7f8c2C
