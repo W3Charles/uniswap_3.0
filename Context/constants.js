2ab8b61c4da851f37b2c7e8b936e7efde5e6fab7
W3Charles <www3charles@gmail.com>
2023-04-03 04:01:59 -0400

    build: import token json files in constants.js

---
+++ Context/constants.js
@@ -0,0 +1,9 @@
+import booToken from "./BooToken.json";
+import lifeToken from "./LifeToken.json";
+import singleSWapToken from "./SingleSwapToken.json";
+import swapMultiHop from "./SwapMultiHop.json";
+
+// BOO deployed to0x9e7F7d0E8b8F38e3CF2b3F7dd362ba2e9E82baa4
+// Life deployed to0x0724F18B2aA7D6413D3fDcF6c0c27458a8170Dd9
+// SingleSwapToken deployed to0xE7FF84Df24A9a252B6E8A5BB093aC52B1d8bEEdf
+// SwapMultiHop deployed to0x6D712CB50297b97b79dE784d10F487C00d7f8c2C
