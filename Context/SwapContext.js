3426520f9113fb9bf7b493eb27a13d99a8a4e90e
W3Charles <www3charles@gmail.com>
2023-04-07 05:15:44 -0400

    fix: Console TypeError abi.map is not a function
    BREAKING CHANGE
    Add .abi to the ER20 abi property
    
    In const contract = new ethers.Contract instance
    
    Error is now gone and Contracts are showing in the Console log

--- Context/SwapContext.js
+++ Context/SwapContext.js
@@ -19,7 +19,60 @@ import ERC20 from './ERC20.json';
 export const SwapTokenContext = React.createContext();
 
 export const SwapTokenContextProvider = ({ children }) => {
+    //USESTATE
+    const [account, setAccount] = useState('');
+    const [ether, setEther] = useState('');
+    const [networkConnect, setNetworkConnect] = useState('');
+    const [weth9, setWeth9] = useState('');
+    const [dai, setDai] = useState('');
+    
+    const [tokenData, setTokenData] = useState([]);
+
+    const addToken = [ 
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        "0x9e7F7d0E8b8F38e3CF2b3F7dd362ba2e9E82baa4",
+        "0x0724F18B2aA7D6413D3fDcF6c0c27458a8170Dd9",
+    ];
+
+    // FETCH DATA - DISPLAY USER WALLET BALANCE, ETC.
+    const fetchingData = async () => {
+        try {
+            //GET USER ACCOUNT
+            const userAccount = await checkIfWalletConnected();
+            setAccount(userAccount);
+
+            //CREATE PROVIDER
+            const web3modal = new Web3Modal();
+            const connection = await web3modal.connect();
+            const provider = new ethers.providers.Web3Provider(connection);
+           
+            //CHECK BALANCE
+            const balance = await provider.getBalance(userAccount);
+            const convertBal = BigNumber.from(balance).toString();
+            const ethValue = ethers.utils.formatEther(convertBal);
+            setEther(ethValue);
+           
+            // ALL TOKEN DATA & BALANCES
+            addToken.map((el, i) => {
+                // GET CONTRACT
+                const contract = new ethers.Contract(el, ERC20.abi, provider);
+                console.log(contract);
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchingData();
+    }, []);
+
+    const swap = "welcome to swap my token"
+
     return (
-        <SwapTokenContext.Provider value={{swap}}>{children}</SwapTokenContext.Provider>
+        <SwapTokenContext.Provider value={{swap}}>
+            {children}
+        </SwapTokenContext.Provider>
     );
 };
